a44e16b7fcb9084486a5e81c0bb373946aa1ee4d
AryaVaidya08 <aryav663@gmail.com>
2026-01-28 15:27:24 -0500

    wallahi starting point

--- src/main/java/frc/robot/subsystems/shooter/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/Hood.java
@@ -19,7 +19,7 @@ import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 public class Hood extends SubsystemBase {
 
   private final TalonFX hoodMotor;
-  //private final CANcoder throughBore; // Powered by CANCoder
+  private final CANcoder throughBore; // Powered by CANCoder
 
   private final LoggedNetworkNumber motorPosition;
   private final LoggedNetworkNumber throughBorePosition;
@@ -39,90 +39,63 @@ public class Hood extends SubsystemBase {
 
 
   public Hood() {
-    //throughBore = new CANcoder(Constants.Shooter.Hood.HOOD_THROUGHBORE_ID);
-    CANcoderConfiguration cc_cfg = new CANcoderConfiguration();
-    cc_cfg.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5;
-    //throughBore.getConfigurator().apply(cc_cfg);
+    //1. Initalize CANCoder & Create CANcoderConfiguration (Discountinuty Point is 1)
 
-    hoodMotor = new TalonFX(Constants.Shooter.Hood.HOOD_MOTOR_ID);
 
-    var hoodMotorConfig = new TalonFXConfiguration();
+    //2. Initalize Hood Motor with TalonFX ID
 
-    //hoodMotorConfig.Feedback.FeedbackRemoteSensorID = throughBore.getDeviceID();
-    //hoodMotorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
-    //hoodMotorConfig.Feedback.SensorToMechanismRatio = 1.0;
-    //hoodMotorConfig.Feedback.RotorToSensorRatio = 1.0; //NEED TO CHANGE
+    //3. Create TalonFXConfiguration
 
-    var Slot0Configs = hoodMotorConfig.Slot0;
-    Slot0Configs.kS = Constants.Shooter.Hood.HOOD_kS;
-    Slot0Configs.kP = Constants.Shooter.Hood.HOOD_kP;
-    Slot0Configs.kI = Constants.Shooter.Hood.HOOD_kI;
-    Slot0Configs.kD = Constants.Shooter.Hood.HOOD_kD;
+    //4. Link the Feedback for the Hood Motor Config to the throughbore (Set SensorToMechanismRatio & RotorToSensorRatio to 1.0)
 
-    var motionMagicConfigs = hoodMotorConfig.MotionMagic;
-    motionMagicConfigs.MotionMagicCruiseVelocity = Constants.Shooter.Hood.HOOD_maxV;
-    motionMagicConfigs.MotionMagicAcceleration = Constants.Shooter.Hood.HOOD_maxA;
-    hoodMotor.getConfigurator().apply(hoodMotorConfig);
 
-    var motorTalonFXConfigurator = hoodMotor.getConfigurator();
+    //5. Create PID configs for Slot0 (kP, kI, kD, kS)
+    
 
-    var limitConfigs = new CurrentLimitsConfigs();
-    limitConfigs.StatorCurrentLimit = 120;
-    limitConfigs.SupplyCurrentLimit = 120;
-    limitConfigs.StatorCurrentLimitEnable = true;
-    limitConfigs.SupplyCurrentLimitEnable = true;
-    motorTalonFXConfigurator.apply(limitConfigs);
+    //6. Create Motion Magic Configs inside the Hood Motor set it equal to the values in the Constants file under the Shooter class
 
-    var motorConfigs = new MotorOutputConfigs();
-    motorConfigs.NeutralMode = NeutralModeValue.Brake;
-    motorConfigs.Inverted = InvertedValue.Clockwise_Positive; // Change on testing
-    motorTalonFXConfigurator.apply(motorConfigs);
 
-    throughBorePosition = new LoggedNetworkNumber("Hood Absolute Position", 0.0);
-    motorPosition = new LoggedNetworkNumber("Hood Motor Position", 0.0);
+    //7. Apply the config to the hoodMotor
+
+
+    //8. Get the hoodMotor configurator
+
+
+    //9. Create Current Limit Configs (Supply & Stator)
+    
+
+    //10. Apply Current Limit Configs
+
+
+    //11. Create MotorOutputConfigs and apply Brake Mode for Neutral Mode and Clockwise_positive for Inverted
+    
+
+    //12. Apply the motorOutputConfigs to the configurator from step 8
+    
+
+    //13. Create the LoggedNetworkNumers for the throughBorePosition and motorPosition variables (make sure to set the key and default value as 0.0)
+    
   }
 
   public void createNewConfig() {
-    var hoodMotorConfig = new TalonFXConfiguration();
-
-    //hoodMotorConfig.Feedback.FeedbackRemoteSensorID = throughBore.getDeviceID();
-    //hoodMotorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
-    //hoodMotorConfig.Feedback.SensorToMechanismRatio = 1.0;
-    //hoodMotorConfig.Feedback.RotorToSensorRatio = 1.0; //NEED TO CHANGE
-
-    var Slot0Configs = hoodMotorConfig.Slot0;
-    Slot0Configs.kS = Constants.Shooter.Hood.HOOD_kS;
-    Slot0Configs.kP = Constants.Shooter.Hood.HOOD_kP;
-    Slot0Configs.kI = Constants.Shooter.Hood.HOOD_kI;
-    Slot0Configs.kD = Constants.Shooter.Hood.HOOD_kD;
-
-    var motionMagicConfigs = hoodMotorConfig.MotionMagic;
-    motionMagicConfigs.MotionMagicCruiseVelocity = Constants.Shooter.Hood.HOOD_maxV;
-    motionMagicConfigs.MotionMagicAcceleration = Constants.Shooter.Hood.HOOD_maxA;
-    hoodMotor.getConfigurator().apply(hoodMotorConfig);
   }
 
   public double getThroughPosition() {
-    return 0.0;//throughBore.getAbsolutePosition().getValueAsDouble();
+    return 0.0; //0.0 temporarily
   }
 
   public double getMotorPosition() {
-    return hoodMotor.getPosition().getValueAsDouble();
+    return 0.0; //0.0 temporarily
   }
 
   public void moveToPos(double pos) {
-    final MotionMagicVoltage mmRequest = new MotionMagicVoltage(pos);
-    hoodMotor.setControl(mmRequest.withPosition(pos));
   }
 
   public void stopMotor() {
-    hoodMotor.setVoltage(0);
   }
 
   @Override
   public void periodic() {
-    throughBorePosition.set(getThroughPosition());
-    motorPosition.set(getMotorPosition());
   }
 
   @Override
